328886e9ec92a553a732ed6f7f0d17bacfd9b2dc
Göran Sander <mountaindude@ptarmiganlabs.com>
2023-10-26 06:01:55 +0000

    feat(connection-test): Add command to test connection to Sense server Implements #328

---
+++ src/lib/cmd/testconnection.js
@@ -0,0 +1,33 @@
+const { logger, setLoggingLevel, isPkg, execPath } = require('../../globals');
+const { getAboutFromQseow } = require('../util/about');
+
+const testConnection = async (options) => {
+    try {
+        // Set log level
+        setLoggingLevel(options.logLevel);
+
+        logger.verbose(`Ctrl-Q was started as a stand-alone binary: ${isPkg}`);
+        logger.verbose(`Ctrl-Q was started from ${execPath}`);
+
+        logger.info(`Testing connection to Qlik Sense server ${options.host} on port ${options.port}`);
+        logger.debug(`Options: ${JSON.stringify(options, null, 2)}`);
+
+        const aboutInfo = await getAboutFromQseow(options);
+
+        if (!aboutInfo) {
+            logger.error(`Could not get about info from QSEoW`);
+            return false;
+        }
+
+        logger.info(`Successfully connected to Qlik Sense server ${options.host} on port ${options.port}`);
+        logger.info(`Qlik Sense build version: ${aboutInfo.buildVersion}`);
+        logger.info(`Qlik Sense build date: ${aboutInfo.buildDate}`);
+        return true;
+    } catch (err) {
+        logger.error(`EXPORT APP: ${err.stack}`);
+    }
+};
+
+module.exports = {
+    testConnection,
+};
