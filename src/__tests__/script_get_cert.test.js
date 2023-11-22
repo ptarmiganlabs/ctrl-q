855b714022ca56027f704f3007f3f3b3e5e5d86b
Göran Sander <mountaindude@ptarmiganlabs.com>
2023-11-22 13:17:04 +0000

    build: Add unit tests for script-get command  ...

---
+++ src/__tests__/script_get_cert.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+const { test, expect, describe } = require('@jest/globals');
+
+const { getScript } = require('../lib/cmd/getscript');
+
+const options = {
+    logLevel: process.env.CTRL_Q_LOG_LEVEL || 'info',
+    authType: process.env.CTRL_Q_AUTH_TYPE || 'cert',
+    authCertFile: process.env.CTRL_Q_AUTH_CERT_FILE || './cert/client.pem',
+    authCertKeyFile: process.env.CTRL_Q_AUTH_CERT_KEY_FILE || './cert/client_key.pem',
+    host: process.env.CTRL_Q_HOST || '',
+    port: process.env.CTRL_Q_PORT || '4747',
+    virtualProxy: process.env.CTRL_Q_VIRTUAL_PROXY || '',
+    secure: process.env.CTRL_Q_SECURE || true,
+    schemaVersion: process.env.CTRL_Q_SCHEMA_VERSION || '12.612.0',
+    appId: process.env.CTRL_Q_APP_ID || 'a3e0f5d2-000a-464f-998d-33d333b175d7',
+    authUserDir: process.env.CTRL_Q_AUTH_USER_DIR || '',
+    authUserId: process.env.CTRL_Q_AUTH_USER_ID || '',
+};
+
+const defaultTestTimeout = process.env.CTRL_Q_TEST_TIMEOUT || 120000; // 2 minute default timeout
+console.log(`Jest timeout: ${defaultTestTimeout}`);
+jest.setTimeout(defaultTestTimeout);
+
+// Get app script
+describe('get app script (cert auth)', () => {
+    test('Verify parameters', async () => {
+        expect(options.authCertFile).not.toHaveLength(0);
+        expect(options.authCertKeyFile).not.toHaveLength(0);
+        expect(options.host).not.toHaveLength(0);
+        expect(options.authUserDir).not.toHaveLength(0);
+        expect(options.authUserId).not.toHaveLength(0);
+    });
+
+    /**
+     * Get app script
+     * Should succeed
+     */
+    test('get app script', async () => {
+        const result = await getScript(options);
+
+        expect(result.appId).toBe('a3e0f5d2-000a-464f-998d-33d333b175d7');
+        expect(result.appCreatedDate).toBe('2021-06-03T22:04:52.283Z');
+        expect(result.appModifiedDate).toBe('2023-05-05T06:17:05.456Z');
+        expect(result.appScript.length).toBe(1989);
+    });
+});
